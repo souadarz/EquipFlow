b72d2c07f594481567003c05203ad5eacaefef05
soaud arziki <arziki.souad@gmail.com>
2026-03-13 14:41:01 +0000

    configuration de typescript-config, base.json et nextjs.json, creation de packages/shared, types, enums et interfaces

---
+++ apps/api/src/users/shemas/user.shema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import { Role } from '@repo/shared';
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+  @Prop({ required: true, unique: true })
+  email!: string;
+
+  @Prop({ required: true })
+  password!: string;
+
+  @Prop()
+  fullname!: string;
+
+  @Prop()
+  role!: Role;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
