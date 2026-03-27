3df674dcebe60eb8805d001c4795cd3aeb0fbf6b
soaud arziki <arziki.souad@gmail.com>
2026-03-27 17:40:35 +0100

    cookie -fix -4

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+      },
+    ];
+  },
 };
-
-export default nextConfig;
